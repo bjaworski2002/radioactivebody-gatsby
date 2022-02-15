6b6c291a6692a5c0efc3f949b14efc6a0d215a18
bjaworski2002 <bjaworski2002@gmail.com>
2022-02-15 21:18:38 +0100

    Header refactor

--- src/Components/SEO.js
+++ src/Components/SEO.js
@@ -1,25 +1,31 @@
 import React from "react"
-import {Helmet} from "react-helmet"
+import { Helmet } from "react-helmet"
 import { graphql, StaticQuery } from "gatsby"
 import fav from "../assets/favicon.svg"
-const SEO = ({title, description, author, favicon}) => {
+
+const SEO = ({ title, description, author, favicon }) => {
   return (
     <StaticQuery query={detailsQuery} render={data => {
       const metaDescription = description || data.site.siteMetadata.description
       const metaTitle = title || data.site.siteMetadata.title
       const metaAuthor = author || data.site.siteMetadata.author
-      const metaFavicon = favicon || data.site.siteMetadata.favicon
       return (
         <Helmet title={metaTitle} meta={[
-          {name: `description`, content: `${metaDescription}`},
-          {property: `og:title`, content: `${metaTitle}`},
-        ]}>
+          { property: `og:description`, content: `${metaDescription}` },
+          { property: `og:title`, content: `${metaTitle}` },
+          { property: `og:type`, content: `website` },
+          { name: `twitter:card`, content: `summary` },
+          { name: `twitter:creator`, content: metaAuthor },
+          { name: `twitter:title`, content: `${metaTitle}` }
+        ]} htmlAttributes={{ lang: "pl" }}
+        >
+          <meta charSet="utf-8" />
           <link rel="icon" type="image/svg" href={fav} sizes="32x32" />
         </Helmet>
       )
     }} />
-  )
-}
+      )
+    }
 export default SEO
 const detailsQuery = graphql`
   query DefaultSEOQuery {
