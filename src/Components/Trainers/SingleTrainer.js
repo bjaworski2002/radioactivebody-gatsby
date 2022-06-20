8620b5ea20fdb3c187346dfcc2d523cb5c7a87aa
bjaworski2002 <bjaworski2002@gmail.com>
2022-06-20 18:32:24 +0200

    Display full description

--- src/Components/Trainers/SingleTrainer.js
+++ src/Components/Trainers/SingleTrainer.js
@@ -2,28 +2,36 @@ import React, { useEffect } from "react"
 import styled from "styled-components"
 import { up, down, between, only } from "styled-breakpoints"
 import { StaticImage } from "gatsby-plugin-image"
-import {SubTitle, SubText} from "../../styles/globalStyled"
+import { SubTitle, SubText } from "../../styles/globalStyled"
 
 export default function SingleTrainer(props) {
   useEffect(() => {
     console.log(props.data.id)
   }, [])
   return (
-    <MainCont isEven={props.index % 2 === 0}>
-      <TextCont isEven={props.index % 2 === 0} data-aos={props.index % 2 === 0 ? "fade-left" : "fade-right"}>
-        <SubTitle>{`${props.data.attributes.imie} ${props.data.attributes.nazwisko}`}</SubTitle>
-        <SubText>{props.data.attributes.opis}</SubText>
-        {props.showButtons ? <TrainerHref href={`/trainer?id=${props.data.id}`}>Dowiedz się więcej!</TrainerHref> : null}
-      </TextCont>
-      <ImageOuter isEven={props.index % 2 === 0}>
-        <ImageCont>
-          <img
-            alt=""
-            src={`${props.data.attributes.image.data.attributes.url}`}
-          />
-        </ImageCont>
-      </ImageOuter>
-    </MainCont>
+    <>
+      <MainCont isEven={props.index % 2 === 0}>
+        <TextCont isEven={props.index % 2 === 0} data-aos={props.index % 2 === 0 ? "fade-left" : "fade-right"}>
+          <SubTitle>{`${props.data.attributes.imie} ${props.data.attributes.nazwisko}`}</SubTitle>
+          <SubText>{props.data.attributes.opis.split("\n")[0]}</SubText>
+          {props.showButtons ?
+            <TrainerHref href={`/trainer?id=${props.data.id}`}>Dowiedz się więcej</TrainerHref> : null}
+        </TextCont>
+        <ImageOuter isEven={props.index % 2 === 0}>
+          <ImageCont>
+            <img
+              alt=""
+              src={`${props.data.attributes.image.data.attributes.url}`}
+            />
+          </ImageCont>
+        </ImageOuter>
+      </MainCont>
+      {props.full ?
+        <MainCont>
+          <SubText style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>{props.data.attributes.opis.split("\n\n").map((e, index) => {if (index !== 0) return <TextCont style={{width: '70%', textAlign: 'center', margin: '1rem'}}>{e}</TextCont>})}</SubText>
+        </MainCont>
+        : null}
+    </>
   )
 }
 const MainCont = styled.div`
@@ -33,6 +41,7 @@ const MainCont = styled.div`
   align-items: center;
   color: white;
   flex-direction: ${props => props.isEven ? `row-reverse` : `row`};
+
   ${down("lg")} {
     flex-direction: column-reverse;
   }
@@ -41,16 +50,19 @@ const TextCont = styled.div`
   width: 42rem;
   margin: 2rem;
   text-align: left;
+
   ${only("lg")} {
     margin-left: ${props => props.isEven ? `2rem` : `0`};
     margin-right: ${props => props.isEven ? `0` : `2rem`};
     width: 32rem;
   }
+
   ${down("lg")} {
     margin: 2rem;
     width: calc(100% - 4rem);
     text-align: center;
   }
+
   ${down("sm")} {
     margin: 2rem 0;
     width: calc(100% - 2rem);
@@ -61,13 +73,15 @@ const ImageOuter = styled.div`
   margin: 2rem;
   width: 22rem;
   height: 32rem;
-  ${only("lg")}{
+
+  ${only("lg")} {
     margin-right: ${props => props.isEven ? `2rem` : `0`};
     margin-left: ${props => props.isEven ? `0` : `2rem`};
     width: 18rem;
     height: 26rem;
   }
-  ${down("sm")}{
+
+  ${down("sm")} {
     width: calc(100% - 4rem);
     height: 100vw;
   }
@@ -76,6 +90,7 @@ const ImageCont = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
+
   img {
     position: relative;
     width: 100%;
@@ -101,7 +116,8 @@ const TrainerHref = styled.a`
   background-color: #FFDA54;
   transition: 0.2s;
   opacity: 0.8;
-  :hover{
+
+  :hover {
     opacity: 1;
   }
 `
